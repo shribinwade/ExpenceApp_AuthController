d0b9b0ae40a27f7001f720acec156c2c35532fde
shribinwade <shribinwade.9103@gmail.com>
2026-04-02 02:13:49 +0530

    application.properties file added with db connection

--- app/src/main/java/org/example/Services/UserDetailsServiceImpl.java
+++ app/src/main/java/org/example/Services/UserDetailsServiceImpl.java
@@ -10,9 +10,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 
